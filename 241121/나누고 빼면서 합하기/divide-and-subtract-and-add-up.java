ea6bacf748040fd67f962bbf6b1b22d0d62d07fb
Hanseung Son <121085516+Hanseung2@users.noreply.github.com>
2024-11-21 11:50:41 +0900

    Title: 나누고 빼면서 합하기, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241121/나누고 빼면서 합하기/divide-and-subtract-and-add-up.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static int calculateSum(int[] A, int m) {
+        int sum = 0;
+        
+        while (m > 1) {
+            sum += A[m - 1]; 
+            if (m % 2 == 0) {
+                m /= 2;
+            } else {
+                m -= 1;
+            }
+        }
+        
+        sum += A[m - 1];
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]); // 수열 A의 크기
+        int m = Integer.parseInt(input[1]); // m번째 원소
+        
+        String[] inputA = br.readLine().split(" ");
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(inputA[i]);
+        }
+        
+        int result = calculateSum(A, m);
+        System.out.println(result);
+    }
+}
