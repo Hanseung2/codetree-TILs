4e292856e10871024404b79212ce59082f4ed0ec
Hanseung Son <121085516+Hanseung2@users.noreply.github.com>
2024-11-25 16:57:44 +0900

    Title: 중앙값 계산 2, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

--- 241125/중앙값 계산 2/get-median-2.java
+++ 241125/중앙값 계산 2/get-median-2.java
@@ -23,7 +23,7 @@ public class Main {
         }
         for (int i = 0; i < n; i++) {
             if((i+1)%2!=0){
-                System.out.println(midFunc(elements, i));
+                System.out.print(midFunc(elements, i)+" ");
             }
         }
 
