30fdd66ed51ac28f585ad09c5b2e20f0390dc2c4
Hanseung Son <121085516+Hanseung2@users.noreply.github.com>
2024-11-25 16:56:14 +0900

    Title: 중앙값 계산 2, Time: 85ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241125/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static int midFunc(int[] arr, int idx)
+    {
+        int index = (idx+1)/2;
+        Arrays.sort(arr,0,idx+1);
+        System.out.println(Arrays.toString(arr));
+        return arr[index];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] inputs = br.readLine().split(" ");
+        int[] elements = new int[n];
+        for (int i = 0; i <n; i++) {
+            elements[i] = Integer.parseInt(inputs[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if((i+1)%2!=0){
+                System.out.println(midFunc(elements, i));
+            }
+        }
+
+    }
+}
